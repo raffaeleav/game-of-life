c4a481c3161241dfd83164fecc203d8271f14b0f
Raffaele Aviello <114619463+raffaeleav@users.noreply.github.com>
2025-06-23 18:38:15 +0200

    fix: removed redundant docker pull

--- scripts/start_mpi_container.sh
+++ scripts/start_mpi_container.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# required since the container image is x86_64 (and built-in docker emulation layer doesn't work)
 colima delete -f
 colima start --cpu 2 --disk 10 --arch x86_64
 
-docker pull spagnuolocarmine/docker-mpi:latest
 docker build --no-cache -t spagnuolocarmine/docker-mpi:latest .
 docker run --rm -it --mount src="$(pwd)",target=/home,type=bind spagnuolocarmine/docker-mpi:latest
